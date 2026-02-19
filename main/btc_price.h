9dfa7c9741ba381c7296e744ea4e51077fce46ba
Fruithy <Magnusrobbestad@hotmail.com>
2026-02-19 18:55:00 +0100

    Create btc_price.h

---
+++ main/btc_price.h
@@ -0,0 +1,7 @@
+#ifndef BTC_PRICE_H
+#define BTC_PRICE_H
+
+float get_btc_price();  // returns the latest BTC price in NOK
+void fetch_btc_price(); // fetches the latest price from the API
+
+#endif
